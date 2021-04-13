c7c128869c8d5d00077b39235e49c7e4b1a55862
hbjang <13klove@naver.com>
2021-04-13 15:24:41 +0900

    xy 값 수정

--- src/main/java/com/example/github/GithubApplication.java
+++ src/main/java/com/example/github/GithubApplication.java
@@ -8,6 +8,7 @@ public class GithubApplication {
 
     public static void main(String[] args) {
         Integer x = 0, y = 3;
+        Integer xy = x+y;
         SpringApplication.run(GithubApplication.class, args);
         x = 3;
         y = 0;
